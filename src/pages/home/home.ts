03216db65c556c52d3370cea161bd6c74fabed06
Victor Santos <vsdeveloper96@outlook.com>
2018-10-01 15:47:11 -0300

    Desabilitando menu na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,13 +9,21 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
 
   }
 
   login()
   {
     this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  ionViewWillEnter(){
+    this.menuCtrl.swipeEnable(false);
+  }
+
+  ionViewCanLeave(){
+    this.menuCtrl.swipeEnable(true);
   }
 
 }
