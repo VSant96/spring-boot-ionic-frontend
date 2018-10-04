01f5412aa79965c274710acdadf595b908dcbf94
Victor Santos <vsdeveloper96@outlook.com>
2018-10-04 16:30:39 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems : CartItem[];
   cliente : ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido : string;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
@@ -56,11 +57,16 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home()
+  {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout()
   {
     this.pedidoService.insert(this.pedido)
       .subscribe(response=> {
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
         this.cartService.createOrClearCart();
         
       }, error => {
@@ -71,5 +77,10 @@ export class OrderConfirmationPage {
       });
   }
 
+  private extractId(location: string) : string
+  {
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
+  }
 
 }
