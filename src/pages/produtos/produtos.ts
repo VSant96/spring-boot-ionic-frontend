73a23d5c07a0dd26398a10417f2bd4a143f36c8f
Victor Santos <vsdeveloper96@outlook.com>
2018-10-04 16:45:10 -0300

    Refresh

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -20,6 +20,11 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();  
+  }
+
+  loadData()
+  {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoadingDefault();
     this.produtoService.findByCategoria(categoria_id)
@@ -60,5 +65,12 @@ export class ProdutosPage {
   
     loading.present();
     return loading;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
