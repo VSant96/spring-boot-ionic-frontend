0ddeef3015ec450f204d6dab8920173fbcc5d672
Victor Santos <vsdeveloper96@outlook.com>
2018-10-04 14:22:28 -0300

    Salvando pedidos

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,3 +1,4 @@
+import { PedidoService } from './../../services/domain/pedido.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { ClienteDTO } from './../../models/cliente.dto';
 import { CartService } from './../../services/domain/cart.service';
@@ -22,7 +23,8 @@ export class OrderConfirmationPage {
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public pedidoService : PedidoService) {
     this.pedido = this.navParams.get('pedido');
     
   }
@@ -48,5 +50,26 @@ export class OrderConfirmationPage {
   {
     return this.cartService.total();
   }
+
+  back()
+  {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout()
+  {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response=> {
+        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+        
+      }, error => {
+        if(error.status == 403)
+        {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
 
 }
