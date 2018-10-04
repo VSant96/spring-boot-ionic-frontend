9d53c7d316e9a8c1845d227194ad247566157968
Victor Santos <vsdeveloper96@outlook.com>
2018-10-04 11:37:01 -0300

    Armazenando os dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,16 +1,10 @@
+import { CartService } from './../../services/domain/cart.service';
+import { PedidoDTO } from './../../models/pedido.dto';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { StorageService } from './../../services/storage.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ClienteDTO } from '../../models/cliente.dto';
-
-/**
- * Generated class for the PickAddressPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,11 +14,13 @@ import { ClienteDTO } from '../../models/cliente.dto';
 export class PickAddressPage {
 
   items : EnderecoDTO[];
+  pedido: PedidoDTO;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public storageService : StorageService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public cartService : CartService) {
   }
 
   ionViewDidLoad() {
@@ -33,7 +29,15 @@ export class PickAddressPage {
     {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.items = response['enderecos'];      
+          let cart = this.cartService.getCart();
+          this.items = response['enderecos']; 
+          this.pedido = 
+          {
+            cliente: {id: response['id']},
+            enderecoDeEntrega : null,
+            pagamento: null,
+            items : cart.items.map(x=> {return {quantidade: x.quantidade, produto :{id: x.produto.id}}})
+          };     
         },
         error => {
           if(error.status == 403)
@@ -45,6 +49,12 @@ export class PickAddressPage {
     {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+  nextPage(item : EnderecoDTO)
+  {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 
 }
