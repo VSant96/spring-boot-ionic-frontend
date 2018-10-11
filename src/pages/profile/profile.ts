bf63457b9f1b2882c76c247faa98672c0ea308b9
Victor Santos <vsdeveloper96@outlook.com>
2018-10-11 10:38:45 -0300

    Buxfix - cancelamento da camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -80,7 +80,7 @@ export class ProfilePage {
       this.picture = 'data:image/jpeg;base64,' + imageData;
       this.cameraOn = false;
     }, (err) => {
-     // Handle error
+      this.cameraOn = false;
     });
   }
 
@@ -101,7 +101,7 @@ export class ProfilePage {
       this.picture = 'data:image/jpeg;base64,' + imageData;
       this.cameraOn = false;
     }, (err) => {
-     // Handle error
+      this.cameraOn = false;
     });
   }
 
